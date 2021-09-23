c468aed80ff3c16927ccd8767eea909e172b3567
wogus216 <wogus21694@gmail.com>
2021-09-23 19:55:13 +0900

    ui 반복

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let Box = styled.div`
+  padding: 20px;
+`;
+
+let Name = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.color};
+`;
+
+let dis = styled.div`
+  display: none;
+`;
 
 function Detail(props) {
+  let [alert, setAlert] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(!alert);
+    }, 2000);
+  });
   let { id } = useParams();
   let findProduct = props.shoes.find(function (shoe) {
     return shoe.id == id;
   });
   let history = useHistory();
+
   return (
     <div className="container">
+      <Box>
+        <Name className="red">상세페이지</Name>
+      </Box>
+      {alert === true ? <Alert /> : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -33,6 +59,14 @@ function Detail(props) {
           </button>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Alert() {
+  return (
+    <div className="my-alert">
+      <p>재고가 얼마 남지 않았습니다.</p>
     </div>
   );
 }
