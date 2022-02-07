ce5b9d00230a89abfe8f44861d7329eb6b92a706
wogus216 <wogus21694@gmail.com>
2022-02-07 21:34:52 +0900

    part2 끝

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,92 +1,132 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }));
 let db;
 //몽고 DB 연결
-const MongoClient =require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
-MongoClient.connect('mongodb+srv://sancho:1234@cluster0.bqxrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(error,client){
+
+app.use('/public', express.static('public'));
+MongoClient.connect(
+  'mongodb+srv://sancho:1234@cluster0.bqxrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  function (error, client) {
     // 연결되면 할일
-    if(error)return console.log(error)
+    if (error) return console.log(error);
     //test 라는 데이터베이스 폴더에 연결해주세요
     db = client.db('test');
-//id를 항상 추가
-// db.collection('codingApple').insertOne({이름: 'sancho',_id: 100, age:20}, function(error, res){
+    //id를 항상 추가
+    // db.collection('codingApple').insertOne({이름: 'sancho',_id: 100, age:20}, function(error, res){
 
-//     console.log('1저장완료')
-// });
+    //     console.log('1저장완료')
+    // });
 
-    app.listen(8080, function(){
-        console.log('싼쵸 접속완료')
+    app.listen(8080, function () {
+      console.log('싼쵸 접속완료');
     });
-    
-})
+  }
+);
 
-
-// // 
+// //
 // 누군가가 /pet으로 방문을 하면..
 // pet 관련된 안내문을 띄어주자
 
-app.get('/pet', function(req, res){
-    res.send('펫용품 쇼핑 페이지임.')
+app.get('/pet', function (req, res) {
+  res.send('펫용품 쇼핑 페이지임.');
+});
 
-})
-
-app.get('/beauty', function(req, res){
-    res.send('뷰티용품 쇼핑 페이지임.')
-
-})
+app.get('/beauty', function (req, res) {
+  res.send('뷰티용품 쇼핑 페이지임.');
+});
 // / 은 기본 주소
-app.get('/', function(req, res){
-    console.log('req',req)
-    res.sendFile(__dirname+ '/index.html')
-
-})
+app.get('/', function (req, res) {
+  console.log('req', req);
+  res.render('index.ejs');
+});
 
 app.get('/write', (req, res) => {
-    console.log('req',req)
-    res.sendFile(__dirname+ '/write.html')
-
-})
+  console.log('req', req);
+  res.render('write.ejs');
+});
 
 // 어떤 사람이 /add 경로로 POST요청을 하면...??를 해주세요~
-app.post('/add',(req,res)=>{
-    console.log('req',req.body)
-    res.send('전송완료')
-    // 카운터라는 파일을 찾아 name 중 게시물 갯수라는 찾는다.
-    db.collection('counter').findOne({name: '게시물갯수'},function(err, result){
-console.log('result :',result.totalPost)
-//총 게시물 갯수를 변수에 저장
-let postCnt = result.totalPost
-   // DB에 저장해주세요
-   db.collection('codingApple').insertOne({_id: postCnt+1,제목:req.body.title, 내용:req.body.text,날짜:req.body.date}, function(){
-       console.log('저장완료2');
-    // + counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가 시켜야 됨
-    // set은 변경할 때 사용하는 연산자;
-    // inc는 기존 값에 더해줄 값
-          db.collection('counter').updateOne({name: '게시물갯수'},{ $inc:{totalPost:1}},function(err, result){
-if(err)return console.log(err)
-
-          })
+app.post('/add', (req, res) => {
+  console.log('req', req.body);
+  res.send('전송완료');
+  // 카운터라는 파일을 찾아 name 중 게시물 갯수라는 찾는다.
+  db.collection('counter').findOne(
+    { name: '게시물갯수' },
+    function (err, result) {
+      console.log('result :', result.totalPost);
+      //총 게시물 갯수를 변수에 저장
+      let postCnt = result.totalPost;
+      // DB에 저장해주세요
+      db.collection('codingApple').insertOne(
+        {
+          _id: postCnt + 1,
+          제목: req.body.title,
+          내용: req.body.text,
+          날짜: req.body.date,
+        },
+        function () {
+          console.log('저장완료2');
+          // + counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가 시켜야 됨
+          // set은 변경할 때 사용하는 연산자;
+          // inc는 기존 값에 더해줄 값
+          db.collection('counter').updateOne(
+            { name: '게시물갯수' },
+            { $inc: { totalPost: 1 } },
+            function (err, result) {
+              if (err) return console.log(err);
+            }
+          );
+        }
+      );
+    }
+  );
 });
-    });
- 
 
-
-})
-
-
-// /list로 접속하면 GET요청으로 접속하면 
+// /list로 접속하면 GET요청으로 접속하면
 // 실제 DB에 저장된 데이터들로 예쁘게 HTML을 보여줌
-app.get('/list',function(req,response){
-// 디비에 저장된 post라는 collection안의 제목이 뭐인 데이터를 꺼내주세요
-db.collection('codingApple').find().toArray(function(error,res){
-    console.log('결과',res);
-    response.render('list.ejs',{posts: res});
-});
-    
+app.get('/list', function (req, response) {
+  // 디비에 저장된 post라는 collection안의 제목이 뭐인 데이터를 꺼내주세요
+  db.collection('codingApple')
+    .find()
+    .toArray(function (error, res) {
+      console.log('결과', res);
+      response.render('list.ejs', { posts: res });
+    });
 });
 
-app.delete('/delete', function(req,response){
-console.log(response.body)
-})
+app.delete('/delete', function (req, response) {
+  console.log('delete', req.body);
+  // 문자열 정수로 변환
+  req.body._id = parseInt(req.body._id);
+  db.collection('codingApple').deleteOne(req.body, function (err, result) {
+    console.log('삭제완료');
+    response.status(200).send({ message: '성공했습니다.' });
+  });
+});
+
+//detail로 접속하면 detail.ejs 로 보여줌'
+
+app.get('/detail/:id', function (req, response) {
+  db.collection('codingApple').findOne(
+    //파라미터 중 :id라는 뜻
+    { _id: parseInt(req.params.id) },
+    function (err, result) {
+      if (err) return next(err);
+
+      console.log('result', result);
+      response.render('detail.ejs', { data: result });
+    }
+  );
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('404');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('500번대에러');
+});
